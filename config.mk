8a050e6ea4c21115670341f4105d6e00f6cd8dc7
Nicholas Magill <nmagill1979@gmail.com>
2020-08-12 18:57:09 +0100

    NGCam_7.4.104-v1.3_gcam.apk

---
+++ config.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += \
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    packages/apps/GoogleCamera/etc/permissions/com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.xml
